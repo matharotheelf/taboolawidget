cbc2efd6eef3c70fe2eec754178b5af2bfafffb6
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-05-02 16:05:42 +0100

    Article has brand stored to it

--- spec/articleTest.js
+++ spec/articleTest.js
@@ -1,7 +1,7 @@
 describe('Article', function(){
   var article;
 
-  article = new Article('This Article', 'Thumbnail Address', 'Link Address')
+  article = new Article('This Article', 'Thumbnail Address', 'Link Address', 'Brand')
 
   it('Article stores a title', function() {
     expect(article.title).toBe('This Article');
@@ -13,5 +13,9 @@ describe('Article', function(){
 
   it('Article stores link', function() {
     expect(article.link).toBe('Link Address');
+  });
+
+  it('Article stores branding', function() {
+    expect(article.brand).toBe('Brand');
   });
 })
--- src/article.js
+++ src/article.js
@@ -1,7 +1,8 @@
 class Article {
-  constructor(title, thumbnail, link){
+  constructor(title, thumbnail, link, brand){
     this.title = title
     this.thumbnail = thumbnail
     this.link = link
+    this.brand = brand
   }
 }
